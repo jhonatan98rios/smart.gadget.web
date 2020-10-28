82009fa73dc0be2267506ad90fa8420963b56c11
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-10-27 21:04:29 -0300

    environment

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const uri = process.env.MONGO_KEY || require("./auth").uri;
+const uri = process.env.MONGO_KEY
+//const uri = require("./auth").uri;
 
 mongoose.connect(
   uri, {
