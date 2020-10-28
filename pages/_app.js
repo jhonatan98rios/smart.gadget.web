03e946e18debcbd3c25c555f67e13709244ff189
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-10-28 08:38:23 -0300

    Beta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }) {
-  return<Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <h3 style={{
+        position: "absolute",
+        top: '0px',
+        right: '10px',
+        color: 'red'
+      }}>
+        BETA
+      </h3>
+    </>
+  )
 }
 
 export default MyApp
