4bdc1563168d3e42a32df18bc02f8f654d062b4c
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-11-02 00:41:42 -0300

    css adjustment

--- components/home/Info/index.js
+++ components/home/Info/index.js
@@ -6,10 +6,10 @@ function Info({top}){
     <div className={styles.container} style={{top}}>
       <p className={styles.description}>
         <strong> Encontre o computador ideal para você! </strong><br/>
-        Através de inteligência artifícial e processameento de linguagem natural, o Smart Gadget é capaz de identificar qual hardware e software se encaixam em suas necessidades. <br/><br/>
-        É só inserir algumas informações sobre o uso, como por exemplo: <br/> "Preciso de um computador para edição de vídeo", ou até, "Quero um pc para jogar". 
-        <br/><br/>Depois é só pressionar o botão "confirmar" e aguardar que nossos servidores irão trabalhar para encontrar o pc ideal para sua necessidade.
-        <br/><br/> Clique abaixo para começar
+        <div className="only-desktop">Através de inteligência artifícial e processameento de linguagem natural, o Smart Gadget é capaz de identificar qual hardware e software se encaixam em suas necessidades.<br/><br className="only-desktop"/> </div>
+        É só inserir algumas informações, como: <br/>"Preciso de um computador para edição de vídeo", ou até, "Quero um pc para jogar". 
+        <br/><br className="only-desktop"/> Depois é só pressionar o botão "Confirmar" e aguardar que nossos servidores irão trabalhar para encontrar o pc ideal para sua necessidade.
+        <br/>Clique abaixo para começar
       </p>        
     </div>
   );
