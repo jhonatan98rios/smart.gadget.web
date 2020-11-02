9498ecfc0bca6908de44bcf8c9026774bc72d393
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-11-02 00:52:05 -0300

    br added to mobile

--- components/home/Info/index.js
+++ components/home/Info/index.js
@@ -8,8 +8,8 @@ function Info({top}){
         <strong> Encontre o computador ideal para você! </strong><br/>
         <div className="only-desktop">Através de inteligência artifícial e processameento de linguagem natural, o Smart Gadget é capaz de identificar qual hardware e software se encaixam em suas necessidades.<br/><br className="only-desktop"/> </div>
         É só inserir algumas informações, como: <br/>"Preciso de um computador para edição de vídeo", ou até, "Quero um pc para jogar". 
-        <br/><br className="only-desktop"/> Depois é só pressionar o botão "Confirmar" e aguardar que nossos servidores irão trabalhar para encontrar o pc ideal para sua necessidade.
-        <br/>Clique abaixo para começar
+        <br/><br/> Depois é só pressionar o botão "Confirmar" e aguardar que nossos servidores irão trabalhar para encontrar o pc ideal para sua necessidade.
+        <br/><br/>Clique abaixo para começar
       </p>        
     </div>
   );
