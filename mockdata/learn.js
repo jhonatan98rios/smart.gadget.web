da7c18c89f9ba66374bc1c61be24ecdd77efb9c3
Jhonatan Teixeira Rios <jhonatan.rios@the9level.com>
2021-03-31 20:30:17 -0300

    Content added

--- mockdata/learn.js
+++ mockdata/learn.js
@@ -2,23 +2,50 @@ export const learn = [
   {
     title: 'O que é um processador?',
     content: [{
-    image: 'https://macmagazine.com.br/wp-content/uploads/2008/12/30-intel-core2-quad-q6600.png',
-    description: `O processador ou CPU (unidade de processamento central) é o componente que realiza as instruções de um programa baseado em um sistema lógico, aritmético e de entrada e saída de dados. O processador pode ser comparado ao papel do cérebro no corpo humano. O processador tem algumas propriedades básicas que devem ser observadas. O número de nucleos é a quantidade de unidades de processamento central independentes. Alguns processadores ainda podem apresentar threads que são unidades virtuais, ou seja, simuladas e que podem somar o desempenho. O clock é a frequência com que os transistores de um processador abrem e fecham. Frequências maiores normalmente significam maior velocidade de processamento. A frequência é medida em gigahertz (GHz), ou bilhões de ciclos por segundo. O cache é uma memória de acesso rápido do processador.  A litografia refere-se ao tamanho do semicondutor que é usado para fabricar um circuito integrado e é expressa em nanômetro. Quanto menor a litografia, mais moderna é a tecnologia utilizada. O soquete é a interface de comunicação entre o processador e a placa mãe. Alguns processadores ainda apresentam vídeo integrado, ou seja, contém uma parte do chip dedicada a realizar processamento gráfico.`
-   }]
+      image: 'https://macmagazine.com.br/wp-content/uploads/2008/12/30-intel-core2-quad-q6600.png',
+      description: `O processador ou CPU (unidade de processamento central) é o componente que realiza as instruções de um programa baseado em um sistema lógico, aritmético e de entrada e saída de dados. O processador pode ser comparado ao papel do cérebro no corpo humano. O processador tem algumas propriedades básicas que devem ser observadas. O número de nucleos é a quantidade de unidades de processamento central independentes. Alguns processadores ainda podem apresentar threads que são unidades virtuais, ou seja, simuladas e que podem somar o desempenho. O clock é a frequência com que os transistores de um processador abrem e fecham. Frequências maiores normalmente significam maior velocidade de processamento. A frequência é medida em gigahertz (GHz), ou bilhões de ciclos por segundo. O cache é uma memória de acesso rápido do processador.  A litografia refere-se ao tamanho do semicondutor que é usado para fabricar um circuito integrado e é expressa em nanômetro. Quanto menor a litografia, mais moderna é a tecnologia utilizada. O soquete é a interface de comunicação entre o processador e a placa mãe. Alguns processadores ainda apresentam vídeo integrado, ou seja, contém uma parte do chip dedicada a realizar processamento gráfico.`
+    }]
   },
   {
     title: 'O que é uma placa mãe?',
     content: [{
-    image: 'https://img.terabyteshop.com.br/produto/g/placa-mae-asrock-h410m-hdvm2-lga-1200ddr4hdmidvivgam2usb32_103128.png',
-    description: `A placa mãe é o componente que reúne e comunica todos os componentes de um sistema computacional. Além de permitir o tráfego de informação, a placa também alimenta alguns periféricos com a energia elétrica que recebe da fonte do gabinete. As propriedades da placa mãe são: Chipset, que é que um chip embarcado na placa que reúne todas as informações que permitirão seu funcionamento em conjunto com os outros componeentes e periféricos. Soquete é a interface de comunicação entre a CPU e a placa mãe. Cada geração de placas e processadores apresentarão um soquete. As placas modernas apresentam recursos embutidos, como dual bios para recursividade, entrada de rede, saída e entrada de audio e vídeo (dependem de GPU integrada na CPU)`
-   }]
+      image: 'https://img.terabyteshop.com.br/produto/g/placa-mae-asrock-h410m-hdvm2-lga-1200ddr4hdmidvivgam2usb32_103128.png',
+      description: `A placa mãe é o componente que reúne e comunica todos os componentes de um sistema computacional. Além de permitir o tráfego de informação, a placa também alimenta alguns periféricos com a energia elétrica que recebe da fonte do gabinete. As propriedades da placa mãe são: Chipset, que é que um chip embarcado na placa que reúne todas as informações que permitirão seu funcionamento em conjunto com os outros componeentes e periféricos. Soquete é a interface de comunicação entre a CPU e a placa mãe. Cada geração de placas e processadores apresentarão um soquete. As placas modernas apresentam recursos embutidos, como dual bios para recursividade, entrada de rede, saída e entrada de audio e vídeo (dependem de GPU integrada na CPU)`
+    }]
   },
   {
     title: 'O que é a memória RAM?',
     content: [{
-    image: 'https://freepikpsd.com/wp-content/uploads/2019/10/memoria-ram-png-4.png',
-    description: `A memória RAM é responsável por dar velocidade ao sistema. Basicamente, a RAM guarda temporariamente toda a informação que o computador esta utilizando, sejam os softwares e arquivos abertos, músicas e vídeos sendo reproduzidos e jogos rodando. Isso é um fator que a diferencia de um HD, por exemplo, que é mais lento e faz um armazenamento para longo prazo. A memória é volátil, ou seja, só armazena informação enquanto energizada (ligada). Toda vez que o PC é desligado essa memória é apagada.`
-   }]
-  }
+      image: 'https://freepikpsd.com/wp-content/uploads/2019/10/memoria-ram-png-4.png',
+      description: `A memória RAM é responsável por dar velocidade ao sistema. Basicamente, a RAM guarda temporariamente toda a informação que o computador esta utilizando, sejam os softwares e arquivos abertos, músicas e vídeos sendo reproduzidos e jogos rodando. Isso é um fator que a diferencia de um HD, por exemplo, que é mais lento e faz um armazenamento para longo prazo. A memória é volátil, ou seja, só armazena informação enquanto energizada (ligada). Toda vez que o PC é desligado essa memória é apagada.`
+    }]
+  },
+  {
+    title: 'O que é o Disco Rígido (HD/SSD)?',
+    content: [{
+      image: 'https://img.terabyteshop.com.br/produto/g/hd-western-digital-caviar-blue-wd10ezex-1tb-7200rpm-64mb-sata-iii_23626.png',
+      description: `O HD ou Disco Rígido serve para armazenar arquivos, programas, jogos e todo tipo de conteúdo que se deseja manter no computador. O componente é mais um tipo de memória que existe dentro de um PC, semelhante a memória RAM, mas diferentemente dela, consegue manter arquivado o conteúdo mesmo após o computador ser desligado, ou seja, não é volátil. Além dos HDs tradicionais, também existem os SSD ou Solid-State Drive que são a mais nova tecnologia de armazenamento disponível. Diferente do HD, o SSD não possui partes móveis e por isso são muito mais rápidos para carregar, ler e salvar arquivos. Também é muito mais resistente à danos por não possuir partes mecânicas. A principal vantagem do SSD é a sua velocidade. Como o SSD coloca todos os arquivos em módulos de memória flash, com o acesso é incrivelmente muito mais rápido.`
+    }]
+  },
+  {
+    title: 'O que é uma Placa de Vídeo (GPU)?',
+    content: [{
+      image: 'https://img.terabyteshop.com.br/produto/g/placa-de-video-asus-geforce-rtx-3060-12gb-gddr6-192bit-90yv0gb2-m0na00_117862.png',
+      description: `A placa de vídeo é o componente do seu computador responsável por gerar as imagens que você vê na tela, seja essa página da web, um filme ou um game. Essa peça também é conhecida como unidade de processamento gráfico (ou GPU) e podem ser integrados, que fazem parte do processador, ou placas dedicadas, maiores e mais poderosas. As GPUs também são muito utilizadas em aplicações que exigem processamento paralelo, como mineração de criptomoeadas, machine learning, e super computação.`
+    }]
+  },
+  {
+    title: 'O que é uma Fonte de Alimentação',
+    content: [{
+      image: 'https://img.terabyteshop.com.br/produto/g/fonte-gamemax-gm500-500w-80-plus-bronze-pfc-ativo-oem_55894.png',
+      description: `A fonte de energia do computador é responsável por converter a voltagem da energia elétrica, que chega pelas tomadas, em voltagens menores e compatíveis, capazes de ser suportadas pelos componentes do computador. Essa peça gera valores que variam entre 12, 5 e 3,3 volts. `
+    }]
+  },
+  {
+    title: 'O que é o Sistema Operacional',
+    content: [{
+      description: `É um programa ou um conjunto de programas cuja função é gerenciar os recursos do sistema e fornecer uma interface entre o hardware e o usuário. Ele funcioná como um tradutor entre os componentes eletrônicos e as ações do usuário através dos dispositivos de entrada e saída (teclado, mouse, monitor, caixa de som). O SO também oferece uma série de APIs que permitem que os desenvolvedores de softwares comuniquem seus programas aos recursos nativos da máquina, como arquivos, rede, camera e outros periféricos. Os Sistemas Operacionais mais populares atualmente são o Windows 10 (da Microsoft), Mac OS (da Apple) e as distribuições Linux, como o Ubuntu e o Android.`
+    }]
+  },
 ]
 
