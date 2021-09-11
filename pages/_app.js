a2f66282e39d5918fd7f43b8096f38c2a5373da5
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2021-09-11 03:53:52 -0700

    ga implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import '../styles/main.scss'
+import { useRouter } from 'next/router';
+import { useEffect } from   'react'
+
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    /* const handleRouteChange = (url) => {
+      if (typeof window !== 'undefined') {
+        ga.pageview(url)
+      }
+    } */
+
+    const handleRouteChange = (url) => (typeof window !== 'undefined') ? ga.pageview(url) : false
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => router.events.off('routeChangeComplete', handleRouteChange)
+
+  }, [router.events])
+
   return (
     <>
       <Component {...pageProps} />
